f2ac9607e03a5d3f57cf37871f80177ae08a0f33
Juan Carlos Lucena Monje <juancarloslucenamonje@gmail.com>
2024-09-14 15:35:47 +0100

    refactor(envLoader): simplify loading of environment variables

--- envLoader/envLoader.go
+++ envLoader/envLoader.go
@@ -9,10 +9,9 @@ import (
 
 func init() {
 	log.Info("Loading environment variables...")
-	envFile := os.Getenv("ENV_FILE")
 
 	// Load environment variables
-	if err := godotenv.Load(envFile); err != nil {
+	if err := godotenv.Load(os.Getenv("ENV_FILE")); err != nil {
 		log.Warn("No env gotten, maybe it's production. If not, check the file path.")
 	} else {
 		log.Info("Environment variables loaded.")
