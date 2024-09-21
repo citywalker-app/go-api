d43e128f74beb4b1cc13a9070e13c7fe903b2ffd
Juan Carlos Lucena Monje <juancarloslucenamonje@gmail.com>
2024-09-21 12:42:15 +0100

    refactor(user): decrease size of response struct

--- pkg/user/infrastructure/handler/response.go
+++ pkg/user/infrastructure/handler/response.go
@@ -4,6 +4,6 @@ import userdomain "github.com/citywalker-app/go-api/pkg/user/domain"
 
 type Response struct {
 	JWT         string          `json:"jwt"`
-	User        userdomain.User `json:"user"`
 	ConfirmCode string          `json:"confirmCode"`
+	User        userdomain.User `json:"user"`
 }
