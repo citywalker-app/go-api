3c2f6c708578c915444293ea29ec9001f27677e7
Juan Carlos Lucena Monje <juancarloslucenamonje@gmail.com>
2024-09-21 13:11:02 +0100

    feat(city): add city router to server

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"github.com/citywalker-app/go-api/middleware"
+	cityrouter "github.com/citywalker-app/go-api/pkg/city/infrastructure/router"
 	userrouter "github.com/citywalker-app/go-api/pkg/user/infrastructure/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
@@ -12,8 +14,8 @@ func SetupRoutes(app *fiber.App) {
 	auth := app.Group("/user")
 	userrouter.Router(auth)
 
-	// cities := app.Group("/cities", middleware.JWTHandler(), middleware.RedisHandler())
-	// cityrouter.Router(cities)
+	cities := app.Group("/cities", middleware.JWTHandler(), middleware.RedisHandler())
+	cityrouter.Router(cities)
 
 	// travels := app.Group("/travels", middleware.JWTHandler())
 	// travelRouter.Router(travels)
