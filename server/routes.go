5575e5a65846361a285bc5f44d7cc2092dee2861
Juan Carlos Lucena Monje <juancarloslucenamonje@gmail.com>
2024-09-22 15:49:46 +0100

    feat(server): add travel route

--- server/routes.go
+++ server/routes.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/citywalker-app/go-api/middleware"
 	cityrouter "github.com/citywalker-app/go-api/pkg/city/infrastructure/router"
+	travelrouter "github.com/citywalker-app/go-api/pkg/travel/infrastructure/router"
 	userrouter "github.com/citywalker-app/go-api/pkg/user/infrastructure/router"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
@@ -17,6 +18,6 @@ func SetupRoutes(app *fiber.App) {
 	cities := app.Group("/cities", middleware.JWTHandler(), middleware.RedisHandler())
 	cityrouter.Router(cities)
 
-	// travels := app.Group("/travels", middleware.JWTHandler())
-	// travelRouter.Router(travels)
+	travels := app.Group("/travels", middleware.JWTHandler())
+	travelrouter.Router(travels)
 }
